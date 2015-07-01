7acc9c1c85da84dde7f13a2418e3125b9656f46f
parejadan <parejadan@gmail.com>
2015-07-01 12:48:48 -0500

    fixed bugs, using Method of Moments for masking magnitudes in order to have data take a form; need to fine tune detection math to reduce sensitivity

--- src/Monitor.java
+++ src/Monitor.java
@@ -1,7 +1,6 @@
 
 
 import java.util.ArrayList;
-import java.util.Date;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -14,10 +13,12 @@ import org.opencv.objdetect.CascadeClassifier;
 
 import app.ai.lifesaver.imgproc.FindEyes;
 import app.ai.lifesaver.imgproc.Vars;
+import app.ai.lifesaver.stats.Stats;
 
 public class Monitor {
 
-    private static FindEyes fd = new FindEyes();
+    private static FindEyes fd = new FindEyes(); //pupil detection interface
+    private static Stats st = new Stats(); //statistical tools interface
     private static CascadeClassifier mJavaDetector; //java classifier
     private static float mRelativeFS = 0.2f;
     private static int mAbsoluteFS = 0;
@@ -25,8 +26,8 @@ public class Monitor {
     private static double placeVal;
 
     private static double std, key;
-    private static long cloCheckTime;
-    private static long magCheckTime;
+    private static int trnlen, checklen;
+    private static int picksize, subsize; 
     boolean cloFlag;
     boolean magFlag;
     
@@ -35,12 +36,13 @@ public class Monitor {
      * @param ct - percent pupil 
      * @param at - acceptable time eyes can remain closed or not present (milliseconds)
      */
-    public Monitor(CascadeClassifier mJDetector, double ct, double pVal, long tl, long trnT) {
+    public Monitor(CascadeClassifier mJDetector, double ct, double pVal, int trs) {
     	mJavaDetector = mJDetector;
     	closedThresh = ct;
     	placeVal = pVal;
-    	cloCheckTime = tl;
-    	magCheckTime = trnT;
+    	trnlen = trs;
+    	checklen = trnlen;
+    	subsize = picksize = 2*checklen/3;
     }
 	
 	/** From a given image, it locates the largest face and identifies the right pupil position 
@@ -85,7 +87,6 @@ public class Monitor {
         	}
         }
 		
-		//Core.circle(mGray,  pupil,  5,  Vars.EYE_CIRCLE_COLR);
     	Highgui.imwrite("detection.png", mGray);
 
         for (Mat tmp : mv) tmp.release();
@@ -95,99 +96,49 @@ public class Monitor {
         return pupil;
 	}
 
-	int length(Point a, Point b) {
-		//double mag = Math.sqrt( Math.pow(a.x-b.x, 2.0) + Math.pow(a.y-b.y, 2.0) );
-		//System.out.printf("a.x: %f\t|b.x: %f\t|a.y: %f\t|b.y: %f\t|m: %f\n", a.x, b.x, a.y, b.y, mag);
-		return (int) Math.sqrt( Math.pow(a.x-b.x, 2.0) + Math.pow(a.y-b.y, 2.0) );
-	}
+	double length(Point a, Point b) { return (int) Math.sqrt( Math.pow(a.x-b.x, 2.0) + Math.pow(a.y-b.y, 2.0) ); }
 	
-	/** Takes in a list of magnitudes and computes the STD from the key
-	 * @param mgs - list of magnitudes recorded for pupil movement
-	 * @param placeVal - if dealing with hole numbers, set to 1, if dealing with values between
-	 * 	[1,0] set to to the desired decimal place. Ex. .234 for 10ths -> set placeVal to 10 for 23
-	 * @return - array where value at index 0 is std, and value at index 1 is key
-	 */
-	double[] computeKeySTD(ArrayList<Integer> mgs) {
-		double[] sigKey = new double[2]; //sigKey[0] = standard derivation; sigKey[1] = Key
-		int size = mgs.size();
-		/*int[] sortedLST;
-		int size = mgs.size(), min = mgs.get(0), max = mgs.get(0);
-		for (int num : mgs) {
-			if (num > max)
-				max = num;
-			else if (num < min)
-				min = num;
-			System.out.printf("%d,", num);
-		}
-		sortedLST = new int[max-min+1];
-		for (int num : mgs) {//perform count sort to distinguish mode (num is treated as a index)
-			sortedLST[num-min]++;
-			System.out.printf("dex: %d\t|num: %d\t|occurance: %d\n", num-min, num, sortedLST[num-min]);
-		}
-		//locate mode, mx is the maximum occurrence of a number from the original list
-		for (int mx = sortedLST[0], i = 1; i < max-min; i++) {
-			if (sortedLST[i] > mx) {
-				sigMod[1] = min+i; //set mode
-				mx = sortedLST[i];
-			}
-			System.out.printf("mode: %f\n", sigMod[1]);
-		}*/
-		
-		for (int num : mgs) { sigKey[1] += num;System.out.printf("%d,", num);}
-		sigKey[1] /= size;
-
-		//compute sample standard derivation
-		for (int  num : mgs)
-			sigKey[0] += Math.pow(sigKey[1]-num, 2.0); //compute variance using a "key"
-
-		sigKey[0] /= size-1;
-		sigKey[0] = Math.sqrt(sigKey[0]);
-
-		return sigKey;
-	}
-
-	/** Checks if the eye movement falls bellow a standard deviation from  regular eye movement
+	/** Checks if the eye movement falls outside a standard deviation interval from target eye movement
 	 * @param mag - magnitude to check on
-	 * @return true if it falls bellow it, false otherwise
+	 * @return true if it falls outside it, false otherwise
 	 */
-	boolean checkMovement(double mag) { return (mag < key-std) ? true : false; }
+	boolean checkMovement(double mag) { return (mag < key-std || mag > key+std) ? true : false; }
 
 	void train(VideoCapture cam) {
 		ArrayList<Point> tracking = new ArrayList<Point>();
 		Mat frame = new Mat();
 		Point tmp, tmp2;
+		int i;
 		
 		//record frames
-		long mgSTime = new Date().getTime(), cTime = new Date().getTime();
-		while (cTime-mgSTime <= magCheckTime) {
-			//System.out.println(cTime-mgSTime);
+		for (i = 0; i < trnlen; i++) {
 			cam.read(frame);
 			tracking.add(getPupilxy(frame));
 			frame.release();
-			cTime = new Date().getTime();
-		}
-		//get baseline Key and std from training data
-		ArrayList<Integer> mags = new ArrayList<Integer>();
-		double[] sigMod;
-		int i, size = tracking.size();
-		tmp = tracking.get(size-1);
-		for (i = size-2; i >= 0; i--) {
-			tmp2 = tracking.get(i);
-			//treat closed eye signals as no eye movement
-			if (tmp.x == -1)
-				mags.add( length(new Point(0, 0), tmp2) );
-			else if (tmp2.x == -1)
-				mags.add( length(tmp, new Point(0, 0)) );
-			else
-				mags.add( length(tmp, tmp2) );	
-			tmp = tmp2; //rotate coordinate for next computation
 		}
 		
-		sigMod = computeKeySTD(mags);
-		std = sigMod[0];
-		key = sigMod[1];
-		System.out.printf("standard: %f\t|key: %f\n", std, key);
+		//get baseline Key and std from training data
+		ArrayList<Double> mags = new ArrayList<Double>(), moments;
+		tmp = tracking.get(trnlen-1);
+		for (i = trnlen-2; i >= 0; i--) { //compute magnitude with frames
+			tmp2 = tracking.get(i);
+			
+			//treat closed eye signals as no eye movement
+			if (tmp.x == -1) mags.add( length(new Point(0, 0), tmp2) );
+			else if (tmp2.x == -1) mags.add( length(tmp, new Point(0, 0)) );
+			else mags.add( length(tmp, tmp2) );	
+			
+			tmp = tmp2; //rotate coordinate for next computation
+		}
+		System.out.printf("mags: %d\t|trnlen: %d\t|checklen: %d\n", mags.size(), trnlen, checklen);
+		moments = st.genRandMoments(mags, picksize, subsize);
+		key = st.getMean( moments );
+		std = st.getSTD(moments, key);
 
+		//i = 0;
+		//for (i = mags.size()-1; i >= 0; i--)
+			//System.out.printf("%f%s", mags.get(i), (i == 0) ? "\n":",");
+		
 	}
 	
 	/**
@@ -197,71 +148,41 @@ public class Monitor {
 	 * @param cam - pointer to video camera where frames can be extracted from
 	 */
 	void watch(VideoCapture cam) {
-		Records tracking = new Records(magCheckTime);
+		Records tracking = new Records(checklen);
+		ArrayList<Double> mags, moments;
 		Mat frame = new Mat();
-		double clCnt;
-		int i, size, count;
-		Recs tmp, tmp2;
-		
-		long cTime, clSTime = new Date().getTime(), mgSTime = new Date().getTime();
-		while(true) {
-			cam.read(frame); //read another frame
-			tracking.add(getPupilxy(frame), new Date().getTime()); //processes frame for pupil coordinates; record time        	
-			frame.release();
-			size = tracking.size();
-			cTime = tracking.data.get( size-1 ).getT(); //use logged time of most recent frame added as current
+		Point tmp, tmp2;
+		double mean;
 
-			//CLOSED EYES DETECTION UNDER CONSTRUCTION
-			/*if (cTime-clSTime >= cloCheckTime) { //time to check for closed eyes
-				System.out.println("checking closed eyes");
-				for (i = size-1, count = 0, clCnt = 0; i >= 0; i--) { //we look from the most recent frame to to the oldest
-					tmp = tracking.data.get(i);
-					System.out.printf("%f\n",tmp.getP().x);
-					if (cTime-tmp.getT() <= cloCheckTime) {//check if frame for closed eyes apply time interval to check
-						if (tmp.getP().x == -1) {//closed eyes frame detected
-							System.out.println("closed eye!");
-							clCnt++;
-						}
-						count++;
-					} else {
-						break; //frames have either already been checked or too old to mater
-					}
-				}
-				if (clCnt / count >= .6)
-					cloFlag = true; //eyes are closed, flag this bitch up
-				else
-					cloFlag = false; //you got off easy this time
-				clSTime = cTime;
-			}*/
+		for (int j = 0; true; j++) {
+			cam.read(frame);
+			tracking.add(getPupilxy(frame));
+			frame.release();
 			
-			if (cTime-mgSTime >= magCheckTime) { //time to check eye magnitudes
-				ArrayList<Integer> mags = new ArrayList<Integer>();
-				double[] sigKey;
-				//calculate magnitudes
-				tmp = tracking.data.get(size-1);
-				for (i = size-2; i >= 0; i--) {
+			if (j >= checklen) { //if we have the trainning amount of frames
+				mags = new ArrayList<Double>();
+				tmp = tracking.data.get(checklen-1); //get the very last frame on list (considered the most recent one recorded)
+				for (int i = checklen-2; i >= 0; i--) {
 					tmp2 = tracking.data.get(i);
-					//System.out.printf("a.x: %f\t|a.y: %f\t|b.x: %f\t|a.y: %f\n", tmp.getP().x, tmp.getP().y, tmp2.getP().x,tmp.getP().y);
+					
 					//treat closed eye signals as no eye movement
-					if (tmp.getP().x == -1)
-						mags.add( length(new Point(0, 0), tmp2.getP()) );
-					else if (tmp2.getP().x == -1)
-						mags.add( length(tmp.getP(), new Point(0, 0)) );
-					else
-						mags.add( length(tmp.getP(), tmp2.getP()) );	
+					if (tmp.x == -1) mags.add( length(new Point(0, 0), tmp2) );
+					else if (tmp2.x == -1) mags.add( length(tmp, new Point(0, 0)) );
+					else mags.add( length(tmp, tmp2) );	
+					
 					tmp = tmp2; //rotate coordinate for next computation
 				}
-
-				sigKey = computeKeySTD(mags); //get std and key from the monitoring time period
-				if (checkMovement(sigKey[1]))
-					magFlag = true; //eye movement is falling bellow baseline
-				else
-					magFlag = false; //all checks-out; as you were
-				System.out.printf("|checked key: %f\t|%s\n", sigKey[1], (magFlag) ? "bellow baseline!":"");
-
+				
+				moments = st.genRandMoments(mags, picksize, subsize);
+				mean = st.getMean( moments );
+				//sig = st.getSTD(mags, mean);
+				magFlag = checkMovement(mean);
+				
+				System.out.printf("train-m: %f\t|check-m: %f\t\n", key, mean);
+				System.out.printf("train-s: %f\t\n", std);
+				System.out.printf( "%s\n\n", (magFlag) ? "distracted" : "paying attention");
 			}
-			
-			//System.out.printf("%s\t|%s\n", (magFlag) ? "bellow baseline!":"", (cloFlag) ? "wake up!":"");
+
 		}
 	}
 }
--- src/Records.java
+++ src/Records.java
@@ -3,42 +3,19 @@ import org.opencv.core.Point;
 
 import java.util.ArrayList;
 
-class Recs {
-	private long tLog;
-	private Point xy;
-	
-	public Recs(Point pt, long time) {
-		xy = pt;
-		tLog = time;
-	}
-	
-	public long getT() { return tLog; }
-	public Point getP() { return xy; }
-}
-
 public class Records {
-	ArrayList<Recs> data;
-	private long expTime;
-	int size;
+	ArrayList<Point> data;
+	private int expLength;
 	
-	public Records(long expT) {
-		data = new ArrayList<Recs>();
-		expTime = expT;
-		size = 0;
+	public Records(int expL) {
+		data = new ArrayList<Point>();
+		expLength = expL;
 	}
 	
-	public void add (Point pt, long curTime) {
-		data.add( new Recs(pt, curTime) ); //add new record
-		//remove out dated frames that exceed expiration time
-		Recs tmp;
-		while (data.size() > 0) {
-			tmp = data.get(0); //old frames are located at the beginning of the list
-			if ( curTime - tmp.getT() >= expTime ) data.remove(0);
-			else break;
-		}
+	public void add (Point pt) {
+		if (data.size()+1 > expLength)
+			data.remove(0);
+		data.add(pt);	
 	}
-	
-	public ArrayList<Recs> getDat() { return data; } //java returns a reference
-	
-	public int size() { return data.size(); }
+
 }
